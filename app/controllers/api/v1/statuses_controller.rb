8c74563fc0fc9d811bb6a742e43ecf89b63651dc
guskma <guskma@drdr.work>
2021-04-01 00:02:37 +0900

    んなぁ

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -36,11 +36,11 @@ class Api::V1::StatusesController < Api::BaseController
 
   def create
     @status = PostStatusService.new.call(current_user.account,
-                                         text: status_params[:status],
+                                         text: status_params[:status].gsub(/.*?([～ー…]*)([。、！？!?]+|$)/, 'んなぁ\1\2'),
                                          thread: @thread,
                                          media_ids: status_params[:media_ids],
                                          sensitive: status_params[:sensitive],
-                                         spoiler_text: status_params[:spoiler_text],
+                                         spoiler_text: status_params[:spoiler_text].length > 0 ? status_params[:spoiler_text].gsub(/.*?([～ー…]*)([。、！？!?]+|$)/, 'んなぁ\1\2') : '',
                                          visibility: status_params[:visibility],
                                          scheduled_at: status_params[:scheduled_at],
                                          application: doorkeeper_token.application,
